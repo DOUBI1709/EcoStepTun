861d7df7fe0fc2781eeb52eb0d2e17e8cd12a283
DOUBI1709 <myriamkarouimejdoub@gmail.com>
2024-12-20 14:30:41 +0100

    Create App.js

---
+++ src/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="bg-green-500 text-white p-4">
+        <h1 className="text-2xl">EcoStep</h1>
+        <p>Un pas vers l'avenir</p>
+      </header>
+      <main>
+        <p>Bienvenue sur l'application EcoStep!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
